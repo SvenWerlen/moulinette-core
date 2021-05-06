0de13d22e37c027fdeac69e684f6b49496d49fdd
Sven Werlen <sven.werlen@pm.me>
2021-05-06 17:54:53 -0400

    Cleanup

--- modules/moulinette-client.js
+++ modules/moulinette-client.js
@@ -8,8 +8,6 @@ export class MoulinetteClient {
   //static GITHUB_SRC = "http://127.0.0.1:5000/static"
   static SERVER_URL = "https://boisdechet.org/mtte"
   static SERVER_OUT = "https://boisdechet.org/mtte/static/out/"
-  //static SERVER_URL = "https://boisdechet.org/moulinette"
-  //static SERVER_OUT = "https://boisdechet.org/moulinette/static/out/"
   static GITHUB_SRC = "https://raw.githubusercontent.com/SvenWerlen/moulinette-data"
   
   static HEADERS = { 'Accept': 'application/json', 'Content-Type': 'application/json' }
