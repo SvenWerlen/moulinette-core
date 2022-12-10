20474776bd5c5c7f19fb87d34608d54244becbe1
Sven Hesse <drmccoy@drmccoy.de>
2022-12-10 13:37:50 +0100

    Fix indexing when game is not hosted on The Forge

--- modules/moulinette-file-util.js
+++ modules/moulinette-file-util.js
@@ -343,7 +343,7 @@ export class MoulinetteFileUtil {
     files = files.filter(f => f.indexOf("_thumb") < 0) // remove thumbnails
     if(debug) console.log(`Moulinette FileUtil | Pack: ${files.length} assets found.`)
     // special case for ForgeVTT => keep entire path
-    if(ForgeVTT.usingTheForge && source != "public") {
+    if(typeof ForgeVTT !== "undefined" && ForgeVTT.usingTheForge && source != "public") {
       return files.map( (path) => { return decodeURIComponent(path) } )
     } else {
       return files.map( (path) => { return decodeURIComponent(path).split(decodeURIComponent(packPath))[1].substr(1) } ) // remove front /
