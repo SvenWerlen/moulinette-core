6aca239f77ddb9743e0ee5eaf0c3fadacd7c5792
Sven Werlen <sven.werlen@pm.me>
2022-12-21 08:46:21 -0500

    Filepicker - Update to new UI

--- modules/moulinette-filepicker.js
+++ modules/moulinette-filepicker.js
@@ -60,7 +60,7 @@ export class MoulinetteFilePickerUI extends FormApplication {
   constructor(module, options = {}) {
     super();
     this.assetInc = 0
-    this.module = module
+    this.activeModule = module
     this.type = options.type
     this.search = options.search
     this.callback = options.callbackSelect
@@ -82,9 +82,12 @@ export class MoulinetteFilePickerUI extends FormApplication {
   }
   
   async getData() {
+    // color
+    const cloudColor = game.settings.get("moulinette-core", "cloudColor")
+
     // fetch available packs & build publishers
     let publishers = {}
-    let packs = await this.module.instance.getPackList()
+    let packs = await this.activeModule.instance.getPackList()
     packs = packs.sort((a, b) => (a.publisher == b.publisher) ? (a.name > b.name ? 1 : -1) : (a.publisher > b.publisher ? 1 : -1)) // sort by 1) publisher and 2) name
     let assetsCount = 0
     let special = false
@@ -94,12 +97,21 @@ export class MoulinetteFilePickerUI extends FormApplication {
 
       if(p.publisher in publishers) {
         publishers[p.publisher].count += p.count
-        if(p.isRemote) publishers[p.publisher].isRemote = true
+        if(p.isRemote) {
+          publishers[p.publisher].isRemote = true
+        }
       } else {
-        publishers[p.publisher] = { name: p.publisher, count: p.count, isRemote: p.isRemote }
+        publishers[p.publisher] = { name: p.publisher, count: p.count, isRemote: p.isRemote, special: p.special }
       }
     })
-    publishers = Object.values(publishers).filter(p => p.count > 0 && !(this.search && this.search.creator && p.name != this.search.creator)).sort((a,b) => a.name > b.name)
+    // highlight cloud/remote creators based on configuration
+    Object.keys(publishers).forEach(k => {
+      const p = publishers[k]
+      if(p.isRemote && cloudColor == "def") p.class = "cloud"
+      if(p.isRemote && cloudColor == "contrast") p.class = "cloud contrast"
+    })
+
+    publishers = Object.values(publishers).filter(p => p.special || p.count > 0).sort((a,b) => a.name > b.name)
     
     // prepare packs 
     // - cleans packname by removing publisher from pack name to avoid redundancy
@@ -127,26 +139,29 @@ export class MoulinetteFilePickerUI extends FormApplication {
 
     // fetch initial asset list
     const terms = this.search && this.search.terms ? this.search.terms : ""
-    this.assets = await this.module.instance.getAssetList(terms, packIdx, publisher)
+    this.assets = await this.activeModule.instance.getAssetList(terms, packIdx, publisher)
 
     const data = { 
       terms: terms,
       user: await game.moulinette.applications.Moulinette.getUser(),
-      supportsModes: this.module.instance.supportsModes(),
-      supportsThumbSizes: this.module.instance.supportsThumbSizes(),
+      supportsModes: this.activeModule.instance.supportsModes(),
+      supportsThumbSizes: this.activeModule.instance.supportsThumbSizes(),
+      supportsWholeWordSearch: this.activeModule.instance.supportsWholeWordSearch(),
+      hidePacks: assetsCount == 0,
       assetsCount: `${assetsCount.toLocaleString()}${special ? "+" : ""}`,
       assets: this.assets.slice(0, MoulinetteFilePickerUI.MAX_ASSETS),
     }
 
-    const browseMode = game.settings.get("moulinette-core", "browseMode")
-    if(browseMode == "byPub") {
-      data.publishers = publishers
-    } else {
-      data.packs = packs
-    }
+    data.publishers = publishers
+    data.packs = []
+
+    // keep publisher names for up/down key events
+    this.publishers = publishers.map(p => p.name)
 
     // reset initial search
     this.search = null
+    this.selCreator = null
+    this.selPack = -1
       
     return data;
   }
@@ -161,6 +176,14 @@ export class MoulinetteFilePickerUI extends FormApplication {
     // give focus to input text
     html.find("#search").focus();
     
+    // search options
+    html.find(".sOptions a").click(this._onSearchOptions.bind(this))
+
+    // initialize
+    if(game.settings.get("moulinette", "wholeWordSearch")) {
+      html.find(".sOptions a.wholeWord").addClass("active")
+    }
+
     // buttons
     html.find("button").click(this._onClickButton.bind(this))
     html.find(".fvtt-picker").click(this._onClickButton.bind(this))
@@ -175,11 +198,52 @@ export class MoulinetteFilePickerUI extends FormApplication {
     const dMode = game.settings.get("moulinette", "displayMode")
     html.find(`.display-modes .mode-${dMode}`).addClass("active")
     
+    // asset search (filter on creator)
+    const parent = this
+    html.find(".filterList.creators a").click(async function(ev) {
+      event.preventDefault();
+      const source = event.currentTarget;
+      parent._onCreatorSelected($(source).closest("li").data("id"), $(source).closest(".top"))
+    });
+
     // asset search (filter on pack)
-    html.find("select.plist").on('change', this._onPackOrPubSelected.bind(this));
+    html.find(".filterList.packs a").click(async function(ev) {
+      event.preventDefault();
+      const Moulinette = game.moulinette.applications.Moulinette
+      const source = event.currentTarget;
+      parent._onPackSelected($(source).closest("li").data("id"), $(source).closest(".top"));
+    });
+
+    // up / down => select next entry
+    html.find(".filterList.creators").keydown(function(ev) {
+      const kEv = ev.originalEvent
+      if(ev.key == "Tab") {
+        event.preventDefault();
+        html.find(ev.shiftKey ? "#search" : ".filterList.packs").focus()
+      } else if(ev.key == "ArrowDown" || ev.key == "ArrowUp") {
+        event.preventDefault();
+        // index can only be [0..pub.lenth]
+        const idx = Math.max(-1, parent.publishers.indexOf(parent.selCreator))
+        const newIdx = Math.min(Math.max(-1, idx + (ev.key == "ArrowDown" ? 1 : -1)), parent.publishers.length -1)
+        parent._onCreatorSelected(newIdx < 0 ? "-1" : parent.publishers[newIdx], $(event.currentTarget).closest(".top"));
+      }
+    })
+    html.find(".filterList.packs").keydown(function(ev) {
+      const kEv = ev.originalEvent
+      if(ev.key == "Tab") {
+        event.preventDefault();
+        html.find(ev.shiftKey ? ".filterList.creators" : "#search").focus()
+      } else if(ev.key == "ArrowDown" || ev.key == "ArrowUp") {
+        event.preventDefault();
+        // index can only be [0..pack.lenth]
+        const idx = Math.max(-1, parent.packs.findIndex(p => p.id == parent.selPack))
+        const newIdx = Math.min(Math.max(-1, idx + (ev.key == "ArrowDown" ? 1 : -1)), parent.packs.length -1)
+        parent._onPackSelected(newIdx < 0 ? "-1" : parent.packs[newIdx].id, $(event.currentTarget).closest(".top"));
+      }
+    })
     
     // delegate activation to module
-    this.module.instance.activateListeners(html, this.callback, this.type)
+    this.activeModule.instance.activateListeners(html, this.callback, this.type)
     
     // enable expand listeners
     html.find(".expand").click(this._onToggleExpand.bind(this));
@@ -190,15 +254,114 @@ export class MoulinetteFilePickerUI extends FormApplication {
     // adapt footer height to fit
     html.find(".list").css('bottom', "80px")
 
+    const maxHeight = 400
+
+    $(".filterList > li").hover(function() {
+      const $container = $(this),
+          $list = $container.find("ul"),
+          $anchor = $container.find("a"),
+          height = $list.height() * 1.0,   // make sure there is enough room at the bottom
+          multiplier = height / maxHeight; // needs to move faster if list is taller
+
+      // need to save height here so it can revert on mouseout
+      $container.data("origHeight", $container.height());
+
+      // so it can retain it's rollover color all the while the dropdown is open
+      $anchor.addClass("hover");
+
+      // make sure dropdown appears directly below parent list item
+      $list.show().css({ paddingTop: $container.data("origHeight")});
+
+      // don't do any animation if list shorter than max
+      if (multiplier > 1) {
+        $container
+          .css({ height: maxHeight, overflow: "hidden" })
+          .mousemove(function(e) {
+              var offset = $container.offset();
+              var relativeY = ((e.pageY - offset.top) * multiplier) - ($container.data("origHeight") * multiplier);
+              if (relativeY > $container.data("origHeight")) {
+                  $list.css("top", -relativeY + $container.data("origHeight"));
+              };
+          });
+      }
+
+    }, function() {
+      var $el = $(this);
+      // put things back to normal
+      $el
+        .height($(this).data("origHeight"))
+        .find("ul")
+        .css({ top: 0 })
+        .hide()
+        .end()
+        .find("a")
+        .removeClass("hover");
+
+    });
+
     this.html = html
   }
   
   /**
-   * User selected a pack
+   * User selects a creator in the list
    */
-  async _onPackOrPubSelected(event) {
-    this.html.find("#search").val("")
+  async _onCreatorSelected(id, dropDownList) {
+    const Moulinette = game.moulinette.applications.Moulinette
+    this.selCreator = id && id != "-1" ? id : null
+    this.selPack = "-1"
+    this.html.find("#creatorName").text(this.selCreator ? id : game.i18n.localize("mtte.chooseCreator"))
+    dropDownList.height(dropDownList.data("origHeight"))
+    // refresh pack list
+    this.html.find("#packName").text(game.i18n.localize("mtte.choosePack"))
+    let packs = await this.activeModule.instance.getPackList()
+    packs = packs.filter(p => p.count > 0)
+    const assetsCount = packs.reduce((acc, p) => acc + p.count, 0); // count number of assets
+    if(this.selCreator) {
+      packs = Moulinette.optimizePacks(packs.filter(p => p.publisher == id))
+    }
+
+    // color
+    const cloudColor = game.settings.get("moulinette-core", "cloudColor")
+
+    this.packs = []
+    let packList = `<li data-id="-1" class="all"><a>${game.i18n.localize("mtte.allPacks")} (${Moulinette.prettyNumber(assetsCount)})</a></li>`
+    if(this.selCreator) {
+      const packNames = Object.keys(packs).sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
+      for(const p of packNames) {
+        const count = packs[p].reduce((acc, p) => acc + p.count, 0);
+        const ids = packs[p].reduce((acc, p) => acc + (acc.length > 0 ? "," : "") + p.idx, "");
+        const isRemote = packs[p].reduce((remote, p) => remote && p.isRemote, true);
+
+        // highlight cloud/remote assets based on configuration
+        let packClass = ""
+        if(isRemote && cloudColor == "def") packClass = "cloud"
+        if(isRemote && cloudColor == "contrast") packClass = "cloud contrast"
+
+        const packName = Moulinette.prettyText(p)
+        packList += `<li data-id="${ids}" class="${packClass}"><a><i class="fas fa-${isRemote ? "cloud" : "desktop"}"></i> ${Moulinette.prettyText(packName)} (${Moulinette.prettyNumber(count)})</a></li>`
+        // keep pack ids for up/down key event
+        this.packs.push({ id: ids, name: packName})
+      }
+    }
+    packList += `<li class="filler"></li>`
+    this.html.find(".filterList.packs .sub_menu").html(packList)
+
     await this._searchAssets()
+    this.html.find(".filterList.creators").focus()
+  }
+
+  /**
+   * User selects a pack in the list
+   */
+  async _onPackSelected(id, dropDownList) {
+    this.selPack = id
+    if(this.selPack) {
+      const match = this.packs.find(p => p.id == id)
+      this.html.find("#packName").text(match ? match.name : game.i18n.localize("mtte.choosePack"))
+      dropDownList.height(dropDownList.data("origHeight"))
+      await this._searchAssets()
+      this.html.find(".filterList.packs").focus()
+    }
   }
   
   /**
@@ -215,23 +378,33 @@ export class MoulinetteFilePickerUI extends FormApplication {
       this.close()
     }
   }
+
+  /**
+   * User clicked on search option
+   */
+  async _onSearchOptions(event) {
+    event.preventDefault();
+
+    const source = event.currentTarget;
+    if(source.classList.contains("wholeWord")) {
+      $(source).toggleClass("active")
+      // store in settings
+      const wholeWord = $(source).hasClass("active")
+      await game.settings.set("moulinette", "wholeWordSearch", wholeWord)
+      ui.notifications.info(game.i18n.localize(wholeWord ? "mtte.wholeWordEnabled" : "mtte.wholeWordDisabled"));
+      this._searchAssets()
+    }
+  }
   
   /**
    * Refresh the list based on the new search
    */
   async _searchAssets() {
     const searchTerms = this.html.find("#search").val().toLowerCase()
-    const selectedValue = this.html.find(".plist").children("option:selected").val()
-    
-    const browseMode = game.settings.get("moulinette-core", "browseMode")
-    if(browseMode == "byPub") {
-      this.assets = await this.module.instance.getAssetList(searchTerms, -1, selectedValue, this.type)
-    } else {
-      this.assets = await this.module.instance.getAssetList(searchTerms, selectedValue, null, this.type)
-    }
-    
-    const supportsModes = this.module.instance.supportsModes()
-    
+
+    this.assets = await this.activeModule.instance.getAssetList(searchTerms, this.selPack, this.selCreator)
+    const supportsModes = this.activeModule.instance.supportsModes()
+
     this.expand = true // flag to disable expand/collapse
     if(this.assets.length == 0 && searchTerms.length == 0) {
       this.html.find('.list').html(`<div class="error">${game.i18n.localize("mtte.specialSearch")}</div>`)
@@ -242,25 +415,26 @@ export class MoulinetteFilePickerUI extends FormApplication {
     else {
       // browse => show all folders but no asset
       const viewMode = game.settings.get("moulinette", "displayMode")
-      let assetsToShow = supportsModes && viewMode == "browse" ? this.assets.filter(a => a.indexOf('class="folder"') > 0) : this.assets.slice(0, MoulinetteFilePickerUI.MAX_ASSETS).join("")
+      let assetsToShow = supportsModes && viewMode == "browse" ? this.assets.filter(a => a.indexOf('class="folder"') > 0) : this.assets.slice(0, MoulinetteForge.MAX_ASSETS).join("")
       // if only 1 folder, show all assets
       if(assetsToShow.length == 1 && viewMode == "browse") {
         assetsToShow = this.assets
         this.expand = false
       }
-      
+
       if(assetsToShow.length == 0 && viewMode == "browse") {
         this.html.find('.list').html(`<div class="error">${game.i18n.localize("mtte.errorBrowseUpdateModule")}</div>`)
       } else {
         this.html.find('.list').html(assetsToShow)
       }
+
       this.ignoreScroll = supportsModes && viewMode == "browse"
       this.assetInc = 0
     }
-    
+
     // re-enable listeners
     this._reEnableListeners()
-    
+
     // force resize window
     this.setPosition()
   }
@@ -360,6 +534,6 @@ export class MoulinetteFilePickerUI extends FormApplication {
   async _onChangeThumbsizes(event) {
     event.preventDefault();
     const source = event.currentTarget
-    this.module.instance.onChangeThumbsSize(source.classList.contains("plus"))
+    this.activeModule.instance.onChangeThumbsSize(source.classList.contains("plus"))
   }
 }
